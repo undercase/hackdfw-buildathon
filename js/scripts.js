9e6bed0f053c39ce5015a8a40bfbb7d9ad82a3c7
bearcott <bear@bearcott.com>
2017-01-02 02:21:54 -0600

    fixed sponsors and header parallax

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var mountainBackAnimation = anime({
+	targets: $('.background .mountain-back')[0],
+	top: 1200,
+	autoplay: false
+});
+var mountainForeAnimation = anime({
+	targets: $('.background .mountain-fore')[0],
+	top: 800,
+	autoplay: false
+});
+var valleyAnimation = anime({
+	targets: $('.background .valley')[0],
+	top: 400,
+	autoplay: false
+});
+var forestAnimation = anime({
+	targets: $('.background .forest')[0],
+	top: 0,
+	autoplay: false
+});
+var titleAnimation = anime({
+	targets: $('#home .content')[0],
+	top: 400,
+	autoplay: false
+});
+$(window).scroll(function() {
+	if ($(window).scrollTop() <= $('#home').height()*.5) {
+		var amount = $(window).scrollTop()/150;
+		mountainBackAnimation.seek(amount);
+		mountainForeAnimation.seek(amount);
+		valleyAnimation.seek(amount);
+		// forestAnimation.seek(amount);
+		titleAnimation.seek(amount);
+	}
+	// console.log($(window).scrollTop());
+});
